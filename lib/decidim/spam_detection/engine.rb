4803ab591dc9608677fbf1ed6aacac49c5cb1224
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-11-30 21:49:40 +0100

    Remove webpacker methods and routes (#9)

--- lib/decidim/spam_detection/engine.rb
+++ lib/decidim/spam_detection/engine.rb
@@ -8,16 +8,6 @@ module Decidim
     # This is the engine that runs on the public interface of spam_detection.
     class Engine < ::Rails::Engine
       isolate_namespace Decidim::SpamDetection
-
-      routes do
-        # Add engine routes here
-        # resources :spam_detection
-        # root to: "spam_detection#index"
-      end
-
-      initializer "SpamDetection.webpacker.assets_path" do
-        Decidim.register_assets_path File.expand_path("app/packs", root)
-      end
     end
   end
 end
