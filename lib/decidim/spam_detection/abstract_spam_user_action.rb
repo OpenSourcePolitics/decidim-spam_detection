da97abc4d9845b40d7e231b32bed05af90c81151
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-12-20 15:31:09 +0100

    Chores/refactoring (#12)
    * Make instance methods class methods
    
    * Refactor Service and split it
    
    * Move service to lib
    
    * Rename classes
    
    * Extract if else to factory
    
    * Extract api call from service
    
    * Remove useless constant
    
    * Don't query already unreported or unblocked users

---
+++ lib/decidim/spam_detection/abstract_spam_user_action.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require "uri"
+require "net/http"
+
+module Decidim
+  module SpamDetection
+    class AbstractSpamUserAction
+      SPAM_USER = {
+        name: ENV.fetch("SPAM_DETECTION_NAME", "spam detection bot"),
+        nickname: ENV.fetch("SPAM_DETECTION_NICKNAME", "Spam_detection_bot"),
+        email: ENV.fetch("SPAM_DETECTION_EMAIL", "spam_detection_bot@opensourcepolitcs.eu")
+      }.freeze
+
+      include Decidim::FormFactory
+
+      def initialize(user, probability)
+        @user = user
+        @probability = probability
+        @moderator = moderation_user
+      end
+
+      def self.call(user, probability)
+        new(user, probability).run
+      end
+
+      def run
+        raise NotImplementedError
+      end
+
+      def moderation_user
+        moderation_admin_params = {
+          name: SPAM_USER[:name],
+          nickname: SPAM_USER[:nickname],
+          email: SPAM_USER[:email],
+          admin: true,
+          organization: @user.organization
+        }
+
+        moderation_admin = Decidim::User.find_by(moderation_admin_params)
+
+        return moderation_admin unless moderation_admin.nil?
+
+        create_moderation_admin(moderation_admin_params)
+      end
+
+      def create_moderation_admin(params)
+        password = ::Devise.friendly_token(::Devise.password_length.last)
+        additional_params = {
+          password: password,
+          password_confirmation: password,
+          tos_agreement: true,
+          email_on_notification: false,
+          email_on_moderations: false
+        }
+        moderation_admin = Decidim::User.new(params.merge(additional_params))
+        moderation_admin.skip_confirmation!
+        moderation_admin.save
+        moderation_admin
+      end
+
+      def add_spam_detection_metadata!(metadata)
+        @user.update!(extended_data: @user.extended_data
+                                        .dup
+                                        .deep_merge("spam_detection" => metadata))
+      end
+    end
+  end
+end
---
+++ spec/lib/decidim/spam_detection/abstract_spam_user_action_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module Decidim
+  module SpamDetection
+    describe AbstractSpamUserAction do
+      let(:subject) { described_class.new(user, 0.0) }
+      let(:organization) { create(:organization) }
+      let!(:user) { create(:user, organization: organization) }
+
+      describe "#run" do
+        it "raises an error" do
+          expect { subject.run }.to raise_error(NotImplementedError)
+        end
+      end
+
+      describe "#moderation_user" do
+        it "creates the admin" do
+          expect { subject.moderation_user }.to change(Decidim::User, :count)
+        end
+
+        context "when moderation admin exists" do
+          let!(:moderation_admin) do
+            create(:user,
+                   :admin,
+                   organization: organization,
+                   name: "spam detection bot",
+                   nickname: "Spam_detection_bot",
+                   email: "spam_detection_bot@opensourcepolitcs.eu")
+          end
+
+          it "reuses the admin" do
+            expect { subject.moderation_user }.not_to change(Decidim::User, :count)
+          end
+        end
+
+        describe "#spam detection metadata" do
+          it "#add spam detection metadata" do
+            subject.add_spam_detection_metadata!({ "foo" => "bar" })
+
+            expect(user.reload.extended_data["spam_detection"]).to eq({ "foo" => "bar" })
+          end
+
+          context "when extended_data already exist" do
+            it "doesn't overrides it" do
+              user.update!(extended_data: { "other": "data" })
+              subject.add_spam_detection_metadata!({ "foo" => "bar" })
+
+              expect(user.reload.extended_data).to eq({ "other" => "data", "spam_detection" => { "foo" => "bar" } })
+            end
+          end
+
+          context "when key already exists" do
+            it "overrides it" do
+              user.update!(extended_data: { "spam_detection" => { "foo" => "bar" } })
+              subject.add_spam_detection_metadata!({ "foo" => "barz" })
+              expect(user.reload.extended_data["spam_detection"]).to eq({ "foo" => "barz" })
+            end
+          end
+        end
+      end
+    end
+  end
+end
