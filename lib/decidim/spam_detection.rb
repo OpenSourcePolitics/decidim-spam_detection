b81ffa2a22def4b520a379b9ba1f27cb10005b2a
Armand Fardeau <armandfardeau@users.noreply.github.com>
2021-12-20 23:52:35 +0100

    Properly autoload constant (#14)

--- lib/decidim/spam_detection.rb
+++ lib/decidim/spam_detection.rb
@@ -8,5 +8,10 @@ module Decidim
   # This namespace holds the logic of the `SpamDetection` component. This component
   # allows users to create spam_detection in a participatory space.
   module SpamDetection
+    autoload :ApiProxy, "decidim/spam_detection/api_proxy"
+    autoload :AbstractSpamUserAction, "decidim/spam_detection/abstract_spam_user_action"
+    autoload :ReportSpamUserAction, "decidim/spam_detection/report_spam_user_action"
+    autoload :BlockSpamUserAction, "decidim/spam_detection/block_spam_user_action"
+    autoload :SpamUserActionFactory, "decidim/spam_detection/spam_user_action_factory"
   end
 end
