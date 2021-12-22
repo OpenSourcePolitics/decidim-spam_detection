8f02f856385d19ea4b66bd98961d8c16efe21b80
Armand Fardeau <fardeauarmand@gmail.com>
2021-12-23 00:01:50 +0100

    Ignore version

--- lib/decidim/spam_detection/version.rb
+++ lib/decidim/spam_detection/version.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# :nocov:
 module Decidim
   # This holds the decidim-spam_detection version.
   module SpamDetection
@@ -8,3 +9,4 @@ module Decidim
     end
   end
 end
+# :nocov:
