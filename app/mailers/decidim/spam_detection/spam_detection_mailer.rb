105b6d4b073725413d391b76f5ee1c08dd9d1cdd
Armand Fardeau <armandfardeau@users.noreply.github.com>
2022-02-24 14:48:17 +0100

    Feature/traduction (#30)
    * Add translation for mailers
    
    * Add translation in bo
    
    * adapt translations
    
    * Fix locales
    
    Co-authored-by: virgile-dev <virgile.deville@gmail.com>

--- app/mailers/decidim/spam_detection/spam_detection_mailer.rb
+++ app/mailers/decidim/spam_detection/spam_detection_mailer.rb
@@ -3,6 +3,8 @@
 module Decidim
   module SpamDetection
     class SpamDetectionMailer < Decidim::ApplicationMailer
+      helper_method :blocked_users_url, :reported_users_url
+
       def notify_detection(user, results)
         with_user(user) do
           @reported_count = results[:reported_user]
@@ -13,6 +15,24 @@ module Decidim
           subject = I18n.t("notify_detection.subject", scope: "decidim.spam_detection_mailer")
           mail(to: @user.email, subject: subject)
         end
+      end
+
+      private
+
+      def blocked_users_url
+        decidim_admin.moderated_users_url(blocked: true, host: root_url)
+      end
+
+      def reported_users_url
+        decidim_admin.moderated_users_url(blocked: false, host: root_url)
+      end
+
+      def decidim_admin
+        Decidim::Admin::Engine.routes.url_helpers
+      end
+
+      def root_url
+        decidim.root_url(host: @user.organization.host)[0..-2]
       end
     end
   end
