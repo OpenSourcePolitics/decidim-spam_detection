fb2ee4624b728ce6f73603bfb84eda1d9b4e04d4
Quentin Champenois <quentin.champenois7@gmail.com>
2024-10-22 16:26:56 +0200

    release: 4.1.1

--- lib/decidim/spam_detection/version.rb
+++ lib/decidim/spam_detection/version.rb
@@ -5,7 +5,7 @@ module Decidim
   # This holds the decidim-spam_detection version.
   module SpamDetection
     def self.version
-      "4.1.0"
+      "4.1.1"
     end
 
     def self.decidim_version
